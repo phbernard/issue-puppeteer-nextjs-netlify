5e9bea6f29b8caf7cd00252aa1024a5f9496f82f
Philippe Bernard <philippe.bernard@gmail.com>
2021-10-12 23:25:19 +0200

    Enable ES Build, switch to NextJS 10

--- next.config.js
+++ next.config.js
@@ -1,5 +1,41 @@
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
+function useEsbuildMinify(config, options) {
+	const { minimizer } = config.optimization;
+	const terserIndex = minimizer.findIndex(
+		minifier => minifier.constructor.name === 'TerserPlugin',
+	);
+	
+	minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
+}
+
+function useEsbuildLoader(config, options) {
+	const { rules } = config.module;
+	const rule = rules.find(rule => rule.test.test('.js'));
+
+	rule.use = {
+		loader: 'esbuild-loader',
+		options,
+	};
+}
+
 module.exports = {
   reactStrictMode: true,
   target: "experimental-serverless-trace",
-  experimental: { esmExternals: true }
+  webpack: (config, { webpack }) => {
+		config.plugins.push(
+			new webpack.ProvidePlugin({
+				React: 'react',
+			}),
+		);
+
+		useEsbuildMinify(config);
+		useEsbuildLoader(config, {
+			// Specify `tsx` if you're using TypeScript
+			loader: 'jsx',
+			target: 'es2017',
+		});
+
+		return config;
+	},
 }
